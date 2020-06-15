88b5068285da15d1198d47cfe2355ec0ba9c48fa
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-15 15:39:55 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -193,11 +193,50 @@ def player_numbers(team)
    end
 end
 
-def player_stats(players_name)
+def player_stats(name)
   #return hash of player stats
-  
+  game_hash.each do |home_away, team_color_player|
+    team_color_player[:players].each do |player|
+      if player[:player_name] == name
+        # binding.pry
+        return player
+      end
+    end
+  end
 end
  
 def big_shoe_rebounds
   # find player with biggest shoe size 
+  result = 0
+  player_rebound = nil
+  game_hash.each do |home_away, team_color_player|
+    team_color_player[:players].each do |player|
+      if player[:shoe] > result 
+        result = player[:shoe]
+        player_rebound = player[:rebounds]
+      end
+      # binding.pry
+    end
+  end 
+  return player_rebound
 end
+
+# def big_shoe_rebounds
+#   result = 0
+#   final_rebound = 0
+#   # find player with biggest shoe size 
+#   game_hash.each do |home_away, team_color_player|
+      
+#     team_color_player[:players].each do |player|
+#       counter = 0
+#       while counter < team_color_player[:players].count  
+#       if  team_color_player[:players][counter][:shoe] > result
+#          team_color_player[:players][counter][:shoe] = result
+#         team_color_player[:players][counter][:rebounds] = final_rebound
+#       end
+#       counter += 1
+#     end
+#     end
+#   end
+#   return final_rebound
+# end
